14b45970c45bcbb9ccde0e7c70219af3b7f06bad
Jannis Leidel <jannis@leidel.info>
2023-09-06 13:11:10 +0200

    Undo patcher in tests.

--- tests/unit/test_patch.py
+++ tests/unit/test_patch.py
@@ -116,19 +116,49 @@ def test_client_token_string_no_environment_token(monkeypatch, token_cleanup):
     assert " e/" not in token_string
 
 
-def test_new_user_agent():
+@pytest.fixture
+def reset_patch(request):
+    def resetter():
+        from conda.cli import install as cli_install
+
+        for container, name, old_value in (
+            (Context, "user_agent", Context._old_user_agent),
+            (Context, "anaconda_anon_usage", None),
+            (
+                Context,
+                "parameter_names",
+                tuple(
+                    filter(
+                        lambda name: name != "anaconda_anon_usage",
+                        Context.parameter_names,
+                    )
+                ),
+            ),
+            (context, "checked_prefix", None),
+            (cli_install, "check_prefix", context._old_check_prefix),
+        ):
+            if hasattr(container, name):
+                delattr(container, name)
+            if old_value is None:
+                continue
+            setattr(container, name, old_value)
+
+    request.addfinalizer(resetter)
+
+
+def test_new_user_agent(reset_patch):
     with pytest.raises(AttributeError, match="_old_user_agent"):
         assert patch._new_user_agent(context) is not None
     patch.main(plugin=True)
     assert patch._new_user_agent(context) is not None
 
 
-def test_new_user_agent_no_token(monkeypatch):
+def test_new_user_agent_no_token(monkeypatch, reset_patch):
     monkeypatch.setattr(patch, "client_token_string", lambda: "")
     patch.main(plugin=True)
     assert patch._new_user_agent(context) is not None
 
 
-def test_main_already_patched(monkeypatch):
+def test_main_already_patched(monkeypatch, reset_patch):
     monkeypatch.setattr(Context, "_old_user_agent", "test", raising=False)
     assert not hasattr(Context, "anaconda_anon_usage")
