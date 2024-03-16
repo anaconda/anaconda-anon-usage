a854167d95a6809b50a3b67c25d9b557dd49bf6a
Michael Grant <mgrant@anaconda.com>
2024-03-16 16:12:33 -0500

    fix build script

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,13 +5,10 @@ if [ "$NEED_SCRIPTS" != yes ]; then
     exit 0
 fi
 rm ${SP_DIR}/anaconda_anon_usage/plugin.py
-if [ "$SUBDIR" = "noarch" ]; then sdir=python-scripts; else sdir=bin; fi
-mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/${sdir}"
+mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/bin"
 cp "scripts/activate.sh" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
-cp "scripts/post-link.sh" "${PREFIX}/${sdir}/.${PKG_NAME}-post-link.sh"
-cp "scripts/pre-unlink.sh" "${PREFIX}/${sdir}/.${PKG_NAME}-pre-unlink.sh"
-if [ "$SUBDIR" = "noarch" ]; then
-    cp "scripts/activate.bat" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.bat"
-    cp "scripts/post-link.bat" "${PREFIX}/${sdir}/.${PKG_NAME}-post-link.bat"
-    cp "scripts/pre-unlink.bat" "${PREFIX}/${sdir}/.${PKG_NAME}-pre-unlink.bat"
-fi
+cp "scripts/post-link.sh" "${PREFIX}/bin/.${PKG_NAME}-post-link.sh"
+cp "scripts/pre-unlink.sh" "${PREFIX}/bin/.${PKG_NAME}-pre-unlink.sh"
+cp "scripts/activate.bat" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.bat"
+cp "scripts/post-link.bat" "${PREFIX}/bin/.${PKG_NAME}-post-link.bat"
+cp "scripts/pre-unlink.bat" "${PREFIX}/bin/.${PKG_NAME}-pre-unlink.bat"
