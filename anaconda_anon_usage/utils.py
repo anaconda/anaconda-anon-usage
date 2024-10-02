64c73bc85cbbe622902dcd09c94a0b6f3260fd30
David Broadlick <dbroadlick@gmail.com>
2024-10-02 09:21:25 -0500

    Grabbing token from DEFERRED array if exists

--- anaconda_anon_usage/utils.py
+++ anaconda_anon_usage/utils.py
@@ -5,6 +5,7 @@ import os
 import sys
 from os.path import dirname, exists
 from threading import RLock
+from typing import List, Optional
 
 DPREFIX = os.environ.get("ANACONDA_ANON_USAGE_DEBUG_PREFIX") or ""
 DEBUG = bool(os.environ.get("ANACONDA_ANON_USAGE_DEBUG")) or DPREFIX
@@ -123,6 +124,23 @@ def _write_attempt(must_exist, fpath, client_token, emulate_fail=False):
         return WRITE_FAIL
 
 
+def _deferred_exists(fpath: str, deferred_writes: List = DEFERRED) -> Optional[str]:
+    """
+    Check if the deferred token write exists in the DEFERRED write array.
+    If the path must already exist, this helper function determines if the token will be written in the future.
+
+    Args:
+        fpath: The file path to check for.
+        deferred_tokens: The list of deferred tokens to check.
+
+    Returns:
+        The token if it exists, otherwise None.
+    """
+    for _, fp, token in deferred_writes:
+        if fp == fpath:
+            return token
+
+
 def _saved_token(fpath, what, must_exist=None):
     """
     Implements the saved token functionality. If the specified
@@ -131,6 +149,13 @@ def _saved_token(fpath, what, must_exist=None):
     this location. If that fails, return an empty string.
     """
     global DEFERRED
+
+    # If a deferred token exits for the given fpath, return it instead of generating a new one.
+    deferred_token = _deferred_exists(fpath)
+    if deferred_token:
+        _debug("Returning deferred %s token: %s", what, deferred_token)
+        return deferred_token
+
     client_token = ""
     _debug("%s token path: %s", what.capitalize(), fpath)
     if what[0] in READ_CHAOS:
