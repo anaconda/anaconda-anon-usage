1b141ded533449751a3727d88f8819461cf91285
Michael C. Grant <mgrant@anaconda.com>
2025-02-25 10:38:18 -0600

    reformat multiple system token support

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import tempfile
-from os import remove
+from os import mkdir, remove
 from os.path import dirname, join
 
 import pytest
@@ -14,39 +14,53 @@ def aau_token_path():
     return join(tokens.CONFIG_DIR, "aau_token")
 
 
-def _system_token_path():
+def _system_token_path(npaths=1):
     with tempfile.TemporaryDirectory() as tname:
         utils._cache_clear("_search_path", "organization_token", "machine_token")
         tname = tname.replace("\\", "/")
         o_path = c_constants.SEARCH_PATH
-        n_path = (
-            "/tmp/fake/condarc.d/",
-            tname + "/.condarc",
-            tname + "/condarc",
-            tname + "/condarc.d/",
-        )
+        n_path = ("/tmp/fake/condarc.d/",)
+        for k in range(npaths):
+            tdir = join(tname, "t%d" % k)
+            mkdir(tdir)
+            n_path += (tdir + "/.condarc", tdir + "/condarc", tdir + "/condarc.d/")
         c_constants.SEARCH_PATH = n_path
         yield n_path
         c_constants.SEARCH_PATH = o_path
-        utils._cache_clear("_search_path", "organization_token", "machine_token")
+        utils._cache_clear("_search_path", "organization_tokens", "machine_tokens")
+
+
+def _build_tokens(tpath, machine=True):
+    otoken = utils._random_token()
+    with open(dirname(tpath) + "/org_token", "w") as fp:
+        fp.write(otoken + "\n# Anaconda organization token\n")
+    if machine:
+        mtoken = utils._random_token()
+        with open(dirname(tpath) + "/machine_token", "w") as fp:
+            fp.write(mtoken + "\n# Anaconda machine token\n")
+    else:
+        mtoken = None
+    return (otoken, mtoken)
 
 
 @pytest.fixture
 def no_system_tokens():
-    for tpath in _system_token_path():
+    for tpath in _system_token_path(0):
         yield (None, None)
 
 
 @pytest.fixture
 def system_tokens():
-    for tpaths in _system_token_path():
-        otoken = utils._random_token()
-        mtoken = utils._random_token()
-        with open(dirname(tpaths[1]) + "/org_token", "w") as fp:
-            fp.write(otoken + "\n# Anaconda organization token\n")
-        with open(dirname(tpaths[1]) + "/machine_token", "w") as fp:
-            fp.write(mtoken + "\n# Anaconda machine token\n")
-        yield (otoken, mtoken)
+    for tpaths in _system_token_path(1):
+        yield _build_tokens(tpaths[1])
+
+
+@pytest.fixture
+def two_org_tokens():
+    for tpaths in _system_token_path(2):
+        t1 = _build_tokens(tpaths[1], True)
+        t2 = _build_tokens(tpaths[4], False)
+        yield t1 + t2[:1]
 
 
 @pytest.fixture(autouse=True)
